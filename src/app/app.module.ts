09a2f0fc75418583a66688630028c45101eba925
sravannerella <sravannerella007@hotmail.com>
2018-09-04 11:16:28 -0400

    SMS integrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { Sim } from '@ionic-native/sim';
+import { SMS } from '@ionic-native/sms';
 
 @NgModule({
   declarations: [
@@ -26,6 +27,7 @@ import { Sim } from '@ionic-native/sim';
     StatusBar,
     SplashScreen,
     Sim,
+    SMS,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,23 +1,45 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 import { Sim } from '@ionic-native/sim';
+import { SMS } from '@ionic-native/sms';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
 
-  phoneNumber: String;
-  
-  constructor(public navCtrl: NavController, private sim: Sim) {
-      this.phoneNumber = "313244";
-      this.sim.getSimInfo().then( (info) => {
-        this.phoneNumber = info;
-      }, (err) => {
-        this.phoneNumber = "FAILED";
-        console.log(this.phoneNumber);
-      });
-  }
+	phoneNumber: String;
+	alert: any;
+
+	constructor(public navCtrl: NavController, private sim: Sim, private sms: SMS, private alertCtrl: AlertController) {
+		this.phoneNumber = "";
+		this.sim.getSimInfo().then((info) => {
+			this.phoneNumber = info;
+		}, (err) => {
+			this.errorAlert();
+		});
+	}
+
+
+	errorAlert(){
+		this.alert = this.alertCtrl.create({
+			title: "Failed!",
+			subTitle: "Unable to get phone number from your device.",
+			buttons: ["OK"]
+		});
+
+		this.alert.present();
+		console.log("Phone Number Not available");
+	}
+
+
+	sendSMS(){
+		if(this.phoneNumber.toString() !== ""){
+			this.sms.send(this.phoneNumber.toString(), "OTP - Your Phone Number is " + this.phoneNumber.toString());
+		} else{
+			this.errorAlert();
+		}
+	}
 
 }
