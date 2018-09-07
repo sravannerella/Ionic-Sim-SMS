1ba983c940c6ccc350dbf4c931bd2e2c982a37a7
sravannerella <sravannerella007@hotmail.com>
2018-09-07 15:38:50 -0400

    auto otp completion

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { NavController, AlertController, Platform } from 'ionic-angular';
+import { NavController, AlertController, Platform, LoadingController } from 'ionic-angular';
 import { Sim } from '@ionic-native/sim';
-import { SMS } from '@ionic-native/sms';
+// import { SMS } as from '@ionic-native/sms';
 import { AndroidPermissions } from '@ionic-native/android-permissions';
-declare var smslist:any;
+declare var SMS:any;
 
 @Component({
 	selector: 'page-home',
@@ -12,10 +12,46 @@ declare var smslist:any;
 export class HomePage {
 
 	phoneNumber: String;
+	smsList = [];
 	alert: any;
+	otp = [];
+	otpAvailable: boolean = false;
 
-	constructor(public navCtrl: NavController, public platform: Platform, private sim: Sim, private sms: SMS, private alertCtrl: AlertController, private permission: AndroidPermissions) {
+	constructor(public navCtrl: NavController, public platform: Platform, private sim: Sim, private alertCtrl: AlertController, private loadCtrl: LoadingController, private permission: AndroidPermissions) {
 		this.phoneNumber = "";
+
+		document.addEventListener('onSMSArrive', (e:any)=>{
+			let sms = e.data;
+			console.log("GOT THE SMS", sms);
+
+			if(sms.body !== undefined){
+				let substr = sms.body.substring( (sms.body.length - 5) , (sms.body.length) );
+				this.otpAvailable = true;
+				var isnum = /^\d+$/.test(substr);
+
+				if(isnum){
+					this.otp = substr.split('');
+					let loading = this.loadCtrl.create({
+						content: 'Authenticating OTP Code...'
+					});
+					loading.present();
+
+					setTimeout(()=> {
+						loading.dismiss();
+					}, 3000);
+
+					// STOP WATCH
+					SMS.stopWatch( ()=> {
+						console.log("Stopped Watching");
+					}, (err) => {
+						console.log("ERR: ", err);
+					});
+
+				} else {
+					console.log("Failed");
+				}
+			}
+		});
 
 		this.sim.requestReadPermission().then(
 			() => {
@@ -26,36 +62,37 @@ export class HomePage {
 			() => console.log('Permission denied')
 		);
 
+		this.permission.requestPermissions([this.permission.PERMISSION.READ_SMS]).then(
+			() => {
+				if(SMS){
+
+					SMS.startWatch( () => {
+						console.log("Watching");
+					}, (err)=> {
+						console.log("ERR: ", err);
+					});
+
+					let filter = {
+						indexFrom: 0,
+						maxCount: 10
+					}
+					SMS.listSMS(filter, (list)=> {
+						console.log("Got THE SMS: ", list);
+						this.smsList = list;
+					}, (err) => {
+						console.log("Read SMS ERR: ", err);
+					});
+				}
+			}, () => {
+				this.permission.requestPermission(this.permission.PERMISSION.READ_SMS);
+			}
+		);
+
 		this.sim.getSimInfo().then((info) => {
 			console.log("INFO: ", info);
 			this.phoneNumber = info.phoneNumber;
 		}, (err) => {
 			this.errorAlert();
-		});
-	}
-
-	ionViewWillEnter() {
-		
-		this.platform.ready().then( () => {
-			this.permission.checkPermission(this.permission.PERMISSION.READ_SMS).then(
-				(success)=> {
-					console.log("Permission Granted", smslist);
-					if(smslist){
-						let filter = {
-							box: 'inbox',
-							indexFrom: 0,
-							maxCount: 10
-						}
-						smslist.listSMS(filter, (list)=> {
-							console.log("Got THE SMS: ", list);
-						}, (err) => {
-							console.log("Read SMS ERR: ", err);
-						});
-					}
-				}, (err) => {
-					this.permission.requestPermission(this.permission.PERMISSION.READ_SMS);
-				}
-			);
 		});
 	}
 
@@ -77,7 +114,14 @@ export class HomePage {
 
 
 	sendSMS(){
-		return (this.phoneNumber !== undefined) ? (this.sms.send(this.phoneNumber.toString(), "OTP - Your Phone Number is " + this.phoneNumber.toString())) : (this.errorAlert());
+
+		let number = Math.floor(Math.random()*90000) + 10000;
+
+		return (this.phoneNumber !== undefined) ? (SMS.sendSMS(this.phoneNumber.toString(), "OTP - Your Phone Number is " + number, ()=> {
+			console.log('sent');
+		}, () => {
+			console.error("Failed to Send");
+		})) : (this.errorAlert());
 	}
 
 }
